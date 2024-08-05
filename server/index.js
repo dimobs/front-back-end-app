9f6343eadeb8cf3aaa41d232ecbc7e36ecbcc951
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-08-05 23:54:39 +0300

    init server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
 const express = require('express');
-const config = require('');
+const config = require('./config/config.json')[process.env.PORT || 'development'];
+const initDB = require('./config/database');
+const cors = require('./middlewares/cors');
+
+start()
+async function start() {
+    await initDB()
+}
+
+const app = express();
+app.use(express.urlencoded({extended: true})) //form value
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'REST service operational' });
+});
+
+app.listen(config.PORT, () => console.log(`http://localhost:${config.PORT} App is running on `));
