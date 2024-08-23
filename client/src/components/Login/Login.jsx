f5c11a79372ce2ab33807bd439fe4a3837b30c9f
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-08-23 08:19:50 +0300

    auth login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,82 +1,91 @@
-import { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './login.css'
-import { useForm } from '../../hooks/useForm';
-import { useLogin } from '../../hooks/useAuth';
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./login.css";
+import { useForm } from "../../hooks/useForm";
+import { useLogin } from "../../hooks/useAuth";
 
-const INITIAL_VALUES = {email: '', password: '',}
+const INITIAL_VALUES = { email: "", password: "" };
 
 export default function Login() {
+  const navigate = useNavigate();
   const login = useLogin();
-const {values, changeHandler, onSubmit} = useForm(INITIAL_VALUES, ({email, password}) => login(email, password))
+  const { values, changeHandler, onSubmit } = useForm(
+    INITIAL_VALUES,
+   async ({ email, password }) => {
+    try{
+      await login(email, password);
+      navigate('/')
+    } catch(err){
+      console.log(err);
+    } 
+    }
+  );
 
-// const [formValues, setFormValues] = useState(INITIAL_VALUES)
+  // const [formValues, setFormValues] = useState(INITIAL_VALUES)
 
-const inputRef = useRef();
+  const inputRef = useRef();
 
-useEffect(() =>{
-  inputRef.current.focus();
-}, []);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-// const formSubmitHandler = (e) => {
-//   e.preventDefault();
-// }
+  // const formSubmitHandler = (e) => {
+  //   e.preventDefault();
+  // }
 
-// const changeHandler = (e) => {
-//   setFormValues(oldValue => ({
-//     ...oldValue,
-//     [e.target.name]: e.target.type === 'checkbox'
-//     ? e.target.checked
-//     : e.target.value,
-//   }));
-// };
+  // const changeHandler = (e) => {
+  //   setFormValues(oldValue => ({
+  //     ...oldValue,
+  //     [e.target.name]: e.target.type === 'checkbox'
+  //     ? e.target.checked
+  //     : e.target.value,
+  //   }));
+  // };
 
-    return(
-      <form onSubmit={onSubmit}>
+  return (
+    <form onSubmit={onSubmit}>
       <div className="login__section">
-      <div className="login__body">
-<div className="login__container">
-  <div className="form signin">
-    <h2>Login</h2>
-    <div className="inputBox">
-      <input 
-      type="text"
-       id="email"
-       required="required" 
-       name='email'
-       ref={inputRef}
-       value={values.email}
-       onChange={changeHandler}
-       />
-      <i className="fa-regular fa-envelope" />
-      <span>email address</span>
-    </div>
-    <div className="inputBox">
-      <input 
-      type="password" 
-      required="required"
-      name='password' 
-      value={values.password}
-       onChange={changeHandler}
-      />
-      <i className="fa-solid fa-lock" />
-      <span>password</span>
-    </div>
-    <div className="inputBox">
-      <input 
-      type="submit" 
-      value="Login" />
-    </div>
-    <p>
-      Not Registered ?{" "}
-      <Link to="/register" className="create">
-        Create an account
-      </Link>
-    </p>
-  </div>
-</div>
-</div>
-</div>
-</form>
-    )
+        <div className="login__body">
+          <div className="login__container">
+            <div className="form signin">
+              <h2>Login</h2>
+              <div className="inputBox">
+                <input
+                  type="text"
+                  id="email"
+                  required="required"
+                  name="email"
+                  ref={inputRef}
+                  value={values.email}
+                  onChange={changeHandler}
+                />
+                <i className="fa-regular fa-envelope" />
+                <span>email address</span>
+              </div>
+              <div className="inputBox">
+                <input
+                  type="password"
+                  required="required"
+                  name="password"
+                  value={values.password}
+                  onChange={changeHandler}
+                />
+                <i className="fa-solid fa-lock" />
+                <span>password</span>
+              </div>
+              <div className="inputBox">
+                <input type="submit" value="Login" />
+              </div>
+              <p>
+                Not Registered ?{" "}
+                <Link to="/register" className="create">
+                  Create an account
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 }
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -2,15 +2,10 @@ import { login } from "../api/auth-api";
 
 
 export const useLogin = () => {
-    const loginHandler = async ( email, password) => {
-        try {
+    const loginHandler = async (email, password) => {
         const result = await login(email, password);
         console.log(result);
-        
-        }catch (err) {
-                console.error(err.message);
-            }
     }
 
     return loginHandler;
-    }
+}
