5397452206c2478caf78055825059473e67c3958
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-08-30 18:52:16 +0300

    api update

--- client/src/api/item-api.js
+++ client/src/api/item-api.js
@@ -5,12 +5,14 @@ const BASE_URL = 'http://localhost:3030/api/items'
 export const getAll = async () => {return await request.get(BASE_URL)};
 export const getOne = async (itemId) => {return await request.get(`${BASE_URL}/${itemId}`)};
 export const create = async (data) => {return await request.post(BASE_URL, data)};
+export const update = async (itemId, itemData) => {return request.put(`${BASE_URL}/${itemId}`, itemData)};
 export const remove = async (itemId) => {return await request.remove(`${BASE_URL}/${itemId}`)};
 
 const itemsAPI = {
   getAll,
   getOne,
   create,
+  update,
   remove,
 };
 
