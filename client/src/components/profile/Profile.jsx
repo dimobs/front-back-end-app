95fc6baf044010d789e770defbae71c6b5fe85c6
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-09-16 14:32:51 +0300

    profile right side input field

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -27,7 +27,9 @@ const ProfileDetails = () => {
             <div className="profile__form">
               <div className={styles["left"]}>
                 <h3>Account Settings</h3>
-
+        <div>
+            <img src="" alt="user-profile-picture" />
+        </div>
                 <form action="">
                   <div>
                     <div className={styles["row"]}></div>
@@ -39,9 +41,11 @@ const ProfileDetails = () => {
                 <h3>Account Settings</h3>
                 <form onSubmit={onsubmitHandler} action="">
                   <div className={styles["form"]}>
+                    <div className={styles['field']}>
                     <div className={styles[("row", "inputBox")]}>
                       <input
                         type="text"
+                        required
                         name="first_name"
                         ref={inputRef}
                         value={values.first_name}
@@ -52,23 +56,27 @@ const ProfileDetails = () => {
                     <div className={styles[("row", "inputBox")]}>
                       <input
                         type="text"
+                        required
                         name="last_name"
                         value={values.last_name}
                         onChange={changeHandler}
                       />
                       <span>Last Name</span>
                     </div>
+                    </div>
                     <div className={styles[("row", "inputBox")]}>
                       <input
                         type="text"
+                        required="false"
                         name="phone_number"
                         value={values.phone_number}
                         onChange={changeHandler}
                       />
-                      <span>Phone Numb:</span>
+                      <span>Phone N:</span>
                     </div>
-                    <div className={styles[("row", "inputBox")]}>
+                    <div className={styles["row", "inputBox"]}>
                       <input
+                      required='false'
                         type="text"
                         name="bio"
                         value={values.bio}
@@ -77,7 +85,9 @@ const ProfileDetails = () => {
                       <span>Bio:</span>
                     </div>
                     <div className="inputBox">
-                      <input type="submit" value="Save" />
+                      <input 
+                      type="submit" 
+                      value="Save" />
                     </div>
                   </div>
                 </form>
