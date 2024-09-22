face8c20a4e557b11f84ca6450ce95bc1f5b698f
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-09-23 02:04:41 +0300

    png icon, contact form

--- client/src/components/contactUs/ContactUs.jsx
+++ client/src/components/contactUs/ContactUs.jsx
@@ -1,6 +1,14 @@
+import {Link} from 'react-router-dom'
 import styles from "./ContactUsCSS.module.css";
 
 import whiteTick from "../../assets/white_tick.png";
+import phoneIcon from "../../assets/phone_icon.png";
+import emailIcon from "../../assets/email_icon.png";
+import mapIcon from "../../assets/map_icon.png";
+import githubIcon from "../../assets/github_icon.png";
+import linkedInIcon from "../../assets/linkedin_icon.png";
+import facebookIcon from "../../assets/facebook_icon.png";
+
 import { useState } from "react";
 
 const ContactUs = () => {
@@ -60,18 +68,55 @@ const ContactUs = () => {
               </form>
             </div>
             <div className={styles["right"]}>
-            <h3>Contact Information</h3>
-            <p>Reach out to us for further communication.</p>
-                  <div className={styles['info__container']}>
-                    
+                <div>
+              <h3>Contact Information</h3>
+              <p>Reach out to us for further communication.</p>
+              </div>
+              <div className={styles["info__container"]}>
+                <div className={styles["one__section"]}>
+                  <div className={styles["img__container"]}>
+                    <img src={phoneIcon} alt="phone" />
                   </div>
+                  <p>+359 889 670-187</p>
+                </div>
+                <div className={styles["one__section"]}>
+                  <div className={styles["img__container"]}>
+                    <img src={emailIcon} alt="email" />
+                  </div>
+                  <p>d_i_m_o@yahoo.com</p>
+                </div>
+                <div className={styles["one__section"]}>
+                  <div className={styles["img__container"]}>
+                    <img src={mapIcon} alt="email" />
+                  </div>
+                  <p>8000 Burgas, Bulgaria</p>
+                </div>
+              </div>
+              <div className={styles["follow-us"]}>
+                <h3>Follow Us:</h3>
+                <div className={styles["socials"]}>
+                  <Link to={"https://github.com/dimobs"} target="_blank">
+                    <img src={githubIcon} alt="github" />
+                  </Link>
+                  <Link
+                    to={"https://www.linkedin.com/in/dimo-karachorbadzhiev-313418123/"}
+                    target="_blank"
+                  >
+                    <img src={linkedInIcon} alt="linkedin" />
+                  </Link>
+                  <Link
+                    to={"https://www.facebook.com/profile.php?id=711387305"}
+                    target="_blank"
+                  >
+                    <img src={facebookIcon} alt="facebook" />
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </section>
 
-        <section className={styles["contact__container__bottom"]}>
-        
-        </section>
+        <section className={styles["contact__container__bottom"]}></section>
       </div>
     </div>
   );
