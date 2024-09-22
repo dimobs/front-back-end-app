1b774d5451076b60bffdd6ab1ce6cd8fdd368272
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-09-22 22:41:34 +0300

    contact form name field

--- client/src/components/contactUs/ContactUs.jsx
+++ client/src/components/contactUs/ContactUs.jsx
@@ -4,12 +4,29 @@ const ContactUs = () => {
     <div className={styles["contact__section"]}>
       <div className={styles["contact__body"]}>
         <section className={styles["contact__container__top"]}>
-            <div className={styles['head__content']}>
-          <h2>Contact Us</h2>
-          <p>
-            Email us with any questions or inquiries. We would be happy to
-            answer your questions and we'll get in touch as soon as we can!
-          </p>
+          <div className={styles["head__content"]}>
+            <h2>Contact Us</h2>
+            <p>
+              Email us with any questions or inquiries. We would be happy to
+              answer your questions and we'll get in touch as soon as we can!
+            </p>
+          </div>
+          <div className={styles["form__info"]}>
+            <div className={styles["left"]}>
+              <form action="">
+                <div className={styles["row"]}>
+                  <div className={styles["field"]}>
+                    <input 
+                    type="text"
+                    id="name"
+                    name=""                    
+                    />
+                    <span>Name:</span>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className={styles["right"]}></div>
           </div>
         </section>
 
